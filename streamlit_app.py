3fba51e1b3a5eb517f9048b73ce1de0e920c17ca
AnnaliseW <wang.annalise@gmail.com>
2025-10-07 20:09:09 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,5 +55,5 @@ if ingredients_list:
 # NEW SECTION TO DISPLAY SMOOTHIEFOOT NUTRITION INFORMATION
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
    
