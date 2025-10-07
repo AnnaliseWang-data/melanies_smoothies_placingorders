ba0845b58ff2e55177fb7f42f9fe74152053d304
AnnaliseW <wang.annalise@gmail.com>
2025-10-07 20:38:09 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,5 +56,3 @@ if ingredients_list:
         st.success('Your Smoothie is ordered,'+name_on_order+'!',icon ="✅")
 
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-sf_dt = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
